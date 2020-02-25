9621bf8ec28eb5b2a54523dca381b3bb9c1daf78
StaceyAlexMiller90 <staceyalexmiller@gmail.com>
2020-02-25 16:51:21 +0100

    date format updated

--- src/components/MainArticle/MainArticle.js
+++ src/components/MainArticle/MainArticle.js
@@ -1,17 +1,14 @@
 import React, {useState, useEffect} from 'react'
 import ArticleCard from '../ArticleCard/ArticleCard.js'
 import axios from "axios"
-
+import moment from 'moment'
 // array.sort(function(a,b){
 //   // Turn your strings into dates, and then subtract them
 //   // to get a value that is either negative, positive, or zero.
 //   return new Date(b.date) - new Date(a.date);
 // });
-
-
 export default function ArticleList() {
   const [articles, set_articles] = useState([]);
-
   useEffect(() => {
     async function doSomeDataFetching() {
       console.log("I'm gonna fetch some data!");
@@ -19,24 +16,21 @@ export default function ArticleList() {
       const res = await axios.get('http://newsapi.org/v2/top-headlines?' +
       'country=us&' +
       'apiKey=ba192a7497304eb795dcfc1aeb2669bc')
-
       http://newsapi.org/v2/top-headlines?country=us&apiKey=ba192a7497304eb795dcfc1aeb2669bc
       console.log("Got back:", res.data.articles);
       set_articles(res.data.articles)
     }
     doSomeDataFetching()
-    
   }, []);
-
   const deleteArticle = () => {
     set_articles([{}])
   }
-
   return (
     <div>
       <p>Here's a lovely list of articles, for your reading pleasure:</p>
       <button onClick={deleteArticle}>Clear notifications</button>
       {articles.map((article, index) => {
+        const dateFormat = moment(article.publishedAt).format("MMM Do YY")
         return(
           <div>
             <ArticleCard 
@@ -45,7 +39,7 @@ export default function ArticleList() {
             content={article.content} 
             link={article.url} 
             pic={article.urlToImage} 
-            date={article.publishedAt} />
+            date={dateFormat} />
           </div>         
         )
       }
