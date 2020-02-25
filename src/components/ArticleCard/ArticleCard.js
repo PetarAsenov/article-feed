95fcd3537249a460f3ed154a988598d7b6435923
StaceyAlexMiller90 <staceyalexmiller@gmail.com>
2020-02-25 16:44:27 +0100

    Article Card Update

--- src/components/ArticleCard/ArticleCard.js
+++ src/components/ArticleCard/ArticleCard.js
@@ -1,17 +1,29 @@
 import React from "react";
+import Card from 'react-bootstrap/Card'
+import CardDeck from 'react-bootstrap/CardDeck'
+
 
 export default function ArticleCard(props) {
   return (
-    <div className="Article">
-      <h2>{props.title}</h2>
-      <h4>{props.link}</h4>
-      <p>{props.content}</p>
-      <img 
-      
-      src={props.pic}
-      alt="new pic" height="200px" width="400px"
-      />
-      <p>{props.date}</p>
-    </div>
+  <Card>
+    <Card.Img variant="top" src={props.pic} />
+    <Card.Body>
+      <Card.Title>{props.title}</Card.Title>
+      <Card.Text>
+      {props.content}
+      </Card.Text>
+      <Card.Text>
+      <a href={props.link}>Click for full article</a>
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <small className="text-muted">{props.date}</small>
+    </Card.Footer>
+  </Card>
   );
 }
+
+
+
+
+
